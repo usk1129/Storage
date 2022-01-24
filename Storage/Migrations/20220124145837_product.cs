d5774cf5a2374cbe1052107f25ee0a2622eeebe6
usk1129 <k_usko@yahoo.com>
2022-01-24 17:13:49 +0100

    Add project files.

---
+++ Storage/Migrations/20220124145837_product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Storage.Migrations
+{
+    public partial class product : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Shelf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ Storage/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+
+        public DateTime OrderDate { get; set; }
+
+        public string Category { get; set; }
+
+        public string Shelf { get; set; }
+        public int Count { get; set; }
+        public string Description { get; set; }
+    }
+}
