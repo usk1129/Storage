bc695e0a774329ac8f120f0f715bcc5caa858ae0
usk1129 <k_usko@yahoo.com>
2022-01-24 17:14:27 +0100

    mvc

--- Storage/Startup.cs
+++ Storage/Startup.cs
@@ -55,7 +55,7 @@ namespace Storage
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Products}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
